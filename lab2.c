d9357e4f72bd7fe1b44e41c89e963231a21297be
RaiJashan17 <125523346+RaiJashan17@users.noreply.github.com>
2023-02-16 11:45:17 -0800

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,18 @@
+/*------------*/
+/* Jashan Rai */
+/* Lab 2 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+    {
+        printf("\nLab 2.\n\n");
+        printf("Hello World.\n");
+        printf("Hi, Jashan Rai.\n\n");
+        printf("\"I've never been and I don't really care about being cool. It's just an awful lot of time and hair gel wasted.\"\n- Chris Martin, Member of Coldplay\n\n");
+    
+        return EXIT_SUCCESS;
+    }
+/*------------*/
+         
